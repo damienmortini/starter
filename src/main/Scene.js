6e14a7d301481af7ecefa1277e9172345c700b1e
dmmn <ddotmnn+git@gmail.com>
2016-01-19 12:56:54 +0000

    remove light / adjust field of view

--- src/main/Scene.js
+++ src/main/Scene.js
@@ -5,10 +5,7 @@ export default class Scene extends THREE.Scene {
   constructor() {
     super();
 
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-
-    let light = new THREE.PointLight();
-    this.add(light);
+    this.camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 10000);
 
     this.camera.position.z = 5;
     this.controls = new THREE.TrackballControls(this.camera);
