e2e587bfeed9081bcbcf30cee66dd42a1dfc823f
Damien Mortini <damien@Damiens-MacBook-Pro.local>
2016-02-17 17:55:39 +0000

    fix pass names

--- src/main/View.js
+++ src/main/View.js
@@ -15,10 +15,10 @@ export default class View {
     this.effectComposer = new THREE.EffectComposer(this.renderer);
     this.renderPass = new THREE.RenderPass();
     this.effectComposer.addPass(this.renderPass);
-    this.fxaaShader = new THREE.ShaderPass(THREE.FXAAShader);
-    this.effectComposer.addPass(this.fxaaShader);
-    this.copyShader = new THREE.ShaderPass(THREE.CopyShader);
-    this.effectComposer.addPass(this.copyShader);
+    this.fxaaShaderPass = new THREE.ShaderPass(THREE.FXAAShader);
+    this.effectComposer.addPass(this.fxaaShaderPass);
+    this.copyShaderPass = new THREE.ShaderPass(THREE.CopyShader);
+    this.effectComposer.addPass(this.copyShaderPass);
 
     this.effectComposer.passes[this.effectComposer.passes.length - 1].renderToScreen = true;
   }
@@ -27,7 +27,7 @@ export default class View {
     width *= window.devicePixelRatio;
     height *= window.devicePixelRatio;
     this.renderer.setSize(width, height, false);
-    this.fxaaShader.uniforms.resolution.value.set(1 / width, 1 / height);
+    this.fxaaShaderPass.uniforms.resolution.value.set(1 / width, 1 / height);
     this.effectComposer.setSize(width, height);
   }
 
