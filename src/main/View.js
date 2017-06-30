0069e48e9e2de91bcac5f3f1ab3d7a85c51980cf
dmmn <ddotmnn+git@gmail.com>
2017-06-30 18:17:33 +0100

    gl branch

---
+++ src/main/View.js
@@ -0,0 +1,84 @@
+import Matrix4 from "dlib/math/Matrix4.js";
+import GLProgram from "dlib/gl/GLProgram.js";
+import GLMesh from "dlib/gl/GLMesh.js";
+import Camera from "dlib/3d/Camera.js";
+import TrackballController from "dlib/3d/controllers/TrackballController.js";
+
+export default class View {
+  constructor({canvas} = {}) {
+    this.canvas = canvas;
+    this.gl = this.canvas.getContext("webgl", {
+      depth: true,
+      alpha: false,
+      antialias: true
+    });
+
+    this.camera = new Camera();
+
+    this.cameraController = new TrackballController({
+      matrix: this.camera.transform,
+      distance: 5
+    });
+
+    this.gl.clearColor(0, 0, 0, 1);
+    this.gl.enable(this.gl.CULL_FACE);
+    this.gl.cullFace(this.gl.BACK);
+    this.gl.enable(this.gl.DEPTH_TEST);
+
+    this.program = new GLProgram({
+      gl: this.gl,
+      uniforms: [
+        ["transform", new Matrix4()]
+      ],
+      vertexShaderChunks: [
+        ["start", `
+          uniform mat4 projectionView;
+          uniform mat4 transform;
+
+          attribute vec3 normal;
+          attribute vec3 position;
+
+          varying vec3 vNormal;
+        `],
+        ["end", `
+          gl_Position = projectionView * transform * vec4(position, 1.);
+          vNormal = normal;
+        `]
+      ],
+      fragmentShaderChunks: [
+        ["start", `
+          precision highp float;
+          varying vec3 vNormal;
+        `],
+        ["end", `
+          gl_FragColor = vec4(vNormal * .5 + .5, 1.);
+        `]
+      ]
+    });
+
+    this.mesh = new GLMesh({
+      gl: this.gl,
+      positionData: new Float32Array([-0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5]),
+      normalData: new Float32Array([0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0]),
+      indiceData: new Uint16Array([0, 2, 3, 0, 3, 1, 4, 6, 7, 4, 7, 5, 8, 10, 11, 8, 11, 9, 12, 14, 15, 12, 15, 13, 16, 18, 19, 16, 19, 17, 20, 22, 23, 20, 23, 21])
+    });
+  }
+
+  resize(width, height) {
+    this.camera.aspectRatio = width / height;
+  }
+ 
+  update() {
+    this.gl.viewport(0, 0, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+    this.cameraController.update();
+    
+    this.program.use();
+    this.program.uniforms.set("projectionView", this.camera.projectionViewMatrix);
+    this.program.attributes.set(this.mesh.attributes);
+
+    this.mesh.bind();
+    this.mesh.draw();
+  }
+}
--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,9 @@
 import "@webcomponents/custom-elements";
 
+import LoopElement from "dlib/customelements/LoopElement.js";
 import Loader from "dlib/utils/Loader.js";
+
+import View from "./View.js";
 
 let template = document.createElement("template");
 Loader.load("src/main/template.html").then((value) => {
@@ -8,10 +11,32 @@ Loader.load("src/main/template.html").then((value) => {
 });
 
 Loader.onLoad.then(() => {
-  window.customElements.define("dnit-main", class extends HTMLElement {
+  window.customElements.define("dnit-main", class extends LoopElement {
     connectedCallback() {
+      super.connectedCallback();
+      
       let templateClone = document.importNode(template.content, true);
       this.appendChild(templateClone);
+
+      this.canvas = this.querySelector("canvas");
+
+      this.view = new View({canvas: this.canvas});
+
+      this.resize();
+    }
+
+    resize() {
+      let width = this.canvas.offsetWidth;
+      let height = this.canvas.offsetHeight;
+
+      this.canvas.width = width * window.devicePixelRatio;
+      this.canvas.height = height * window.devicePixelRatio;
+
+      this.view.resize(width, height);
+    }
+
+    update() {
+      this.view.update();
     }
   });
 });
