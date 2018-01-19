d27745a8970496fd20efddd72d20659f7144b72b
Damien Mortini <ddotmnn+github@gmail.com>
2018-01-19 13:32:25 +1100

    make sure load promise is only create once

--- src/main/index.js
+++ src/main/index.js
@@ -2,16 +2,16 @@ import "@webcomponents/custom-elements";
 
 import Loader from "dlib/utils/Loader.js";
 
-async function load() {
+const loadPromise = async function() {
   const html = await Loader.load("src/main/template.html");
   const template = document.createElement("template");
   template.innerHTML = html;
   return { template };
-}
+}();
 
 window.customElements.define("dnit-main", class extends HTMLElement {
   connectedCallback() {
-    load().then(({ template }) => {
+    loadPromise.then(({ template }) => {
       let templateClone = document.importNode(template.content, true);
       this.appendChild(templateClone);
     });
