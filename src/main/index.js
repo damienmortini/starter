689e9da3669379757d392e596d14075b2e35a971
Damien Mortini <ddotmnn+github@gmail.com>
2018-01-19 23:51:06 +1100

    change load promise to use new Loader

--- src/main/index.js
+++ src/main/index.js
@@ -1,15 +1,13 @@
 import Loader from "dlib/utils/Loader.js";
 
-const loadPromise = async function() {
-  const template = document.createElement("template");
-  template.innerHTML += `<style>@import "src/main/index.css"</style>`;
-  template.innerHTML += await Loader.load("src/main/template.html");
-  return { template };
-}();
+const LOAD_PROMISE = Promise.all([
+  Loader.load({ value: "src/main/template.html", type: "template" }),
+  Loader.load("src/main/index.css")
+]);
 
 window.customElements.define("dnit-main", class extends HTMLElement {
   connectedCallback() {
-    loadPromise.then(({ template }) => {
+    LOAD_PROMISE.then(([template]) => {
       let templateClone = document.importNode(template.content, true);
       this.appendChild(templateClone);
     });
