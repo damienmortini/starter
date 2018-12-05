2c796c29167329f2fd8064daabce344176e570e2
Damien Mortini <ddotmnn+github@gmail.com>
2018-12-05 12:07:07 +1000

    update dependencies

--- build/index.js
+++ build/index.js
@@ -1,11 +1,6 @@
-window.customElements.define("dnit-main", class extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
+window.customElements.define("dnit-main",class extends HTMLElement{connectedCallback(){this.innerHTML=`
       <style>
         @import "src/main/index.css";
       </style>
       <h1>dnit-main</h1>
-    `;
-  }
-});
-//# sourceMappingURL=index.js.map
+    `}});
