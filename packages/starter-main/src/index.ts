f9126ad65ea4eef07955ac75c03a3809db4805ea
Damien Mortini <1583101+damienmortini@users.noreply.github.com>
2024-10-06 18:28:39 -0400

    update eslint

--- packages/starter-main/src/index.ts
+++ packages/starter-main/src/index.ts
@@ -1,16 +1,17 @@
-import style from './index.css' with { type: 'css' }
-import { html, render } from 'lit-html'
+import { html, render } from 'lit-html';
+
+import style from './index.css' with { type: 'css' };
 
 export class StarterMain extends HTMLElement {
   constructor() {
-    super()
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot!.adoptedStyleSheets = [style]
-    this.#renderHTML()
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot!.adoptedStyleSheets = [style];
+    this.#renderHTML();
   }
 
   #renderHTML() {
-    render(html`<div>starter-main</div>`, this.shadowRoot!)
+    render(html`<div>starter-main</div>`, this.shadowRoot!);
   }
 }
-customElements.define('starter-main', StarterMain)
+customElements.define('starter-main', StarterMain);
